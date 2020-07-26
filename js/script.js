9122af3d8f6f7ba008b8a435c342722f699ad5cf
micomihye <mihye458@gmail.com>
2020-07-26 17:23:54 +0900

    up

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 visualSlider();
 
-
+// slider
 function visualSlider() {
     $('.slider').slick({
         autoplay: true,
-        autoplaySpeed: 1000,
-
-    })
+        autoplaySpeed: 3000,
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next'),
+        dots: true
+    });
 }
